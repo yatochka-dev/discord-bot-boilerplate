79f3af322b96e0c91c9460a58cd19cc767286563
saaasuuukaae <philipchef13@gmail.com>
2022-12-28 17:36:31 +0200

    Comments

--- app/types.py
+++ app/types.py
@@ -1,5 +1,9 @@
 from disnake import ApplicationCommandInteraction
 
+# CommandInteraction type is used for slash commands
+#
+# Example:
+# async def <SLASH_COMMAND_NAME>(self, interaction: CommandInteraction) -> None:
 CommandInteraction = ApplicationCommandInteraction
 
 # ... you can add your custom types and enums here
