5e98f4fd22adc17c487ad6096ff32d222893be77
saaasuuukaae <philipchef13@gmail.com>
2023-01-09 15:43:04 +0200

    Utils

---
+++ app/dantic/main.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+
+
+class EmbedDANT(BaseModel):
+    title: str
+    description: str
+    pre_build_color: str = "default"
+    fields: list["EmbedFieldDANT"] = []
+
+
+class EmbedFieldDANT(BaseModel):
+    name: str
+    value: str
+    inline: bool = False
