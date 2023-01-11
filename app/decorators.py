35a87dedcc545312d71cec72fc8811481bcff871
saaasuuukaae <philipchef13@gmail.com>
2023-01-11 20:36:05 +0200

    Added views and tested db

--- app/decorators.py
+++ app/decorators.py
@@ -8,27 +8,27 @@ from .types import CommandInteraction
 
 def db_required(coro):
     @functools.wraps(coro)
-    async def wrapper(self, interaction: CommandInteraction, *args, **kwargs):
+    async def wrapper(self, inter: CommandInteraction, *args, **kwargs):
         self.bot.logger.debug(
             "Called db_required decorator for command:  {}".format(
-                interaction.application_command.name
+                inter.application_command.name
             )
         )
 
-        invoked_guild = interaction.guild
+        invoked_guild = inter.guild
 
         self.bot.logger.debug("Invoked guild: {}".format(invoked_guild))
 
         invoked_user = (
-            interaction.user
-            if isinstance(interaction.author, disnake.User)
-            else interaction.user._user
+            inter.user
+            if isinstance(inter.author, disnake.User)
+            else inter.user._user
         )
 
         self.bot.logger.debug("Invoked user: {}".format(invoked_user))
 
         invoked_member = (
-            interaction.author if isinstance(interaction.user, disnake.Member) else None
+            inter.author if isinstance(inter.user, disnake.Member) else None
         )
 
         self.bot.logger.debug("Invoked member: {}".format(invoked_member))
@@ -40,14 +40,14 @@ def db_required(coro):
         self.bot.logger.debug("Data: {}".format(data))
 
         if isinstance(data, GuildAndUser):
-            interaction.guild_db = data.guild
-            interaction.user_db = data.user
+            inter.guild_db = data.guild
+            inter.user_db = data.user
 
         else:
-            interaction.member_db = data
-            interaction.guild_db = data.guild
-            interaction.user_db = data.user
+            inter.member_db = data
+            inter.guild_db = data.guild
+            inter.user_db = data.user
 
-        await coro(self, interaction, *args, **kwargs)
+        await coro(self, inter, *args, **kwargs)
 
     return wrapper
